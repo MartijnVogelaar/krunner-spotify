69bfe659fd08fabf1aa03f4ab6d8b33577e6d289
Martijn <Martijn.j.Vogelaar@outlook.com>
2020-11-15 12:56:02 +0100

    Improved Repeat command flow.

--- src/commands/Repeat.py
+++ src/commands/Repeat.py
@@ -1,15 +1,19 @@
 from .Command import Command
-from Config import getCommandName
+from Config import getCommandName, getSetting
 
 class Repeat(Command):
     def __init__(self, spotify):
         super().__init__(getCommandName("REPEAT_COMMAND"), spotify)
 
     def Match(self, query: str):
+        if(getSetting("CASE_SENSITIVE") == "False"):
+            query = query.lower()
         if(query in ["track","context","off"]):
-            return [(self.command + " " + query, "Set repeat state to: " + query + ".", "Spotify", 100, 100, {})]
+            return [(self.command + " " + query, "Set repeat mode to: " + query + ".", "Spotify", 100, 100, {})]
         else:
-            return [(" " , "Invalid mode, repeat mode can be: track, context or off.", "Spotify", 100, 100, {})]
+            return [(self.command + " off", "Set repeat mode to: off.", "Spotify", 100, 100, {}),
+                    (self.command + " context", "Set repeat mode to: context.", "Spotify", 100, 100, {}),
+                    (self.command + " track", "Set repeat mode to: track.", "Spotify", 100, 100, {})]
 
     def Run(self, data: str):
         self.spotify.repeat(data)
